b48a1a4d234529d501bc487999add409237fb40d
east301 <me@east301.net>
2016-04-16 15:47:26 +0900

    [setup.sh] support update of dotfiles, and compiles .zshrc after linking

--- setup.sh
+++ setup.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
 # sets up homeshick
-git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-source $HOME/.homesick/repos/homeshick/homeshick.sh
+homesick_dir=$HOME/.homesick/repos/homeshick
+
+if [ ! -d $homesick_dir ]; then
+    git clone https://github.com/andsens/homeshick.git $homesick_dir
+fi
+source $homesick_dir/homeshick.sh
 
 # adds my customizations
-homeshick clone east301/dotfiles
-homeshick cd dotfiles
-git submodule update --init --recursive
+homeshick cd dotfiles >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    homeshick pull dotfiles
+    git submodule update --recursive
+else
+    homeshick clone east301/dotfiles
+    homeshick cd dotfiles
+    git submodule update --init --recursive
+fi
 
 # apply customizations manually..
 echo
 echo "[INFO] Run the following command manually to apply changes"
-echo "[INFO]  $ source $HOME/.homesick/repos/homeshick/homeshick.sh && homeshick symlink"
+echo "[INFO]  $ source $HOME/.homesick/repos/homeshick/homeshick.sh && homeshick link && zcompile $HOME/.zshrc"
